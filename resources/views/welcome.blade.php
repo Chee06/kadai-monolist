2560fa52212c883f7c58f9f2db681b14e8bdb210
chisato.ito <chisato.ito@rakuten.com>
2018-06-21 07:44:00 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,11 @@
     <div class="cover">
         <div class="cover-inner">
             <div class="cover-contents">
+	
                 <h1>素敵なモノと出会う場所</h1>
+	@if (!Auth::check())
                 <a href="" class="btn btn-success btn-lg">モノリストを始める</a>
+	@endif
             </div>
         </div>
     </div>
